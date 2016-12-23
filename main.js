c029f9ffb940a52feafef6ffccc44fe3c6390fe3
Travis Gorman <thefarbetween@me.com>
2016-12-23 09:25:56 -0600

    parse localStorage on page load

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 console.log('hello Taco!')
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
 	e.preventDefault()
 	const text = this.querySelector('[name=item]').value;
-	const item = {text, done: false }
+	const item = {
+		text, 
+		done: false 
+	}
 	items.push(item)
 	populateList(items, itemsList)
+	localStorage.setItem('items', JSON.stringify(items))
 	this.reset()
 	console.log('item:', item )
 	console.table( items )
@@ -16,15 +20,27 @@ function addItem(e) {
 
 // takes an array of items and an HTML element
 function populateList(plates=[], platesList ) {
-	platesList.innerHTML = plates.map((plate, i) =>{
+	platesList.innerHTML = plates.map((plate, i) => {
 		return `
 		<li>
-			<label for="">${plate.text}</label>
+			<input type="checkbox" data-index=${i} id=item${i} ${plate.done ? 'checked' : ''}/>
+			<label for="item${i}">${plate.text}</label>
 		</li>
 		`;
 	}).join('')
 }
 
+// function toggleDone(e) {
+// 	if (!e.target.matches('input')) return;
+// 	const el = e.target;
+// 	const index = el.dataset.index;
+// 	items[index].done = !items[index].done;
+// 	localStorage.setItem('items', JSON.stringify(items));
+// 	populateList(items, itemsList);
 
+// }
 
+populateList(items, itemsList);
+
+// itemsList.addEventListener('click', toggleDone);
 addItems.addEventListener('submit', addItem);
