c5773139a818199252b7a0d242502e5b452fdffa
Travis Gorman <thefarbetween@me.com>
2016-12-22 23:12:30 -0600

    addItems function creates items array of objects

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 #TacoBox
 
+`form` elements have a `reset` method. You can use this to clear forms instead of setting it to an empty string and all that.
+
+
+`addItem` function that pushes values from input into an `items` array
+
+`populateList`function map over the array of item and return a string HTML
+`<li>` item
---
+++ main.js
@@ -0,0 +1,39 @@
+console.log('hello Taco!')
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
+
+function addItem(e) {
+	e.preventDefault()
+	const text = this.querySelector('[name=item]').value;
+	const item = {
+		text,
+		done: false
+	}
+
+	items.push(item)
+
+	this.reset()
+
+	console.log('item:', item )
+	console.log('items:', items )
+};
+
+
+
+
+// populateList takes an array called `plates` and `platesList`
+// maps over the plates array, setting the platesList's inner HTML to 
+
+// function populateList(plates = [], platesList) {
+// 	platesList.innerHTML = plates.map((plate, i) => {
+// 		return `
+// 			<li>
+// 				<label for="">${plate.text}</label>
+// 			</li>
+// 		`;
+// 	}).join('');
+// }
+
+
+addItems.addEventListener('submit', addItem);
